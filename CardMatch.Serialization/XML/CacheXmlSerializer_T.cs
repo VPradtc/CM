adbddecc5c35ad745f7083b4c0f09e2700cc0330
VPradtc <viacheslav.pradchuk@gmail.com>
2017-01-24 00:12:49 +0200

    Proxy

---
+++ CardMatch.Serialization/XML/CacheXmlSerializer_T.cs
@@ -0,0 +1,33 @@
+﻿using CardMatch.Serialization.Core;
+
+namespace CardMatch.Serialization.XML
+{
+    public class CacheXmlSerializer<T> : ISerializer<T>
+        where T: new()
+    {
+        private XmlSnapshotSerializer<T> _coreSerializer;
+
+        private T _lastSavedVersion;
+
+        public CacheXmlSerializer(XmlSnapshotSerializer<T> coreSerializer)
+        {
+            _coreSerializer = coreSerializer;
+        }
+
+        public void Serialize(T target, string filePath)
+        {
+            _coreSerializer.Serialize(target, filePath);
+            _lastSavedVersion = target;
+        }
+
+        public T Deserialize(string filePath)
+        {
+            if (_lastSavedVersion != null)
+            {
+                return _lastSavedVersion;
+            }
+
+            return _coreSerializer.Deserialize(filePath);
+        }
+    }
+}
